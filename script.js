89a8f18049d02bc0bc4f8e53d662f6263b8b77cb
mmz-pl <mateusz@qsgroup.eu>
2024-05-13 19:02:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,23 @@ function calculateBmi()  {
     console.log(result)
     document.querySelector("#result").innerHTML = result.toFixed(2); 
 
-    if(gender.value == "men"){
-
-
-
-    }else{
-
-        }
+    if(result<16){
+        
+    }else if(result>16 && result<16.99){
+        document.querySelector("#result2").innerHTML = " niedowaga" 
+     }else if(result>17 && result<18.49){
+        document.querySelector("#result2").innerHTML = " wychudzenie" 
+     }else if(result>18.5 && result<24.99){
+        document.querySelector("#result2").innerHTML = " wartość prawidłowa" 
+     }else if(result>25 && result<29.99){
+        document.querySelector("#result2").innerHTML = " nadwaga" 
+     }else if(result>30 && result<34.99){
+        document.querySelector("#result2").innerHTML = " otyłość I stopnia" 
+     }else if(result>35 && result<39.99){
+        document.querySelector("#result2").innerHTML = " otyłość II stopnia" 
+     }else{
+        document.querySelector("#result2").innerHTML = " otyłość III stopnia" 
+     }
 }
 
 

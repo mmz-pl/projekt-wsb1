5fca117df37c9af7ed802627a78f55fde67c58d5
mmz-pl <mateusz@qsgroup.eu>
2024-05-13 14:55:02 +0200

    v2

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let agex = document.querySelector("#age")
-let genderx = document.querySelector("#gender")
-let heightx = document.querySelector("#height")
-let weightx = document.querySelector("#weight")
-
+let age = document.querySelector("#age")
+let gender = document.querySelector("#gender")
+let height = document.querySelector("#height")
+let weight = document.querySelector("#weight")
+let result
 
 document.querySelector('#button').addEventListener('click',calculateBmi)
+
 
 function calculateBmi()  {
    
 
-    let result = weightx.value / ((heightx.value/100)**2)
+    result = weight.value / ((height.value/100)**2)
     console.log(result)
     document.querySelector("#result").innerHTML = result.toFixed(2); 
+
+    if(gender.value == "men"){
+
+
+
+    }else{
+
+        }
 }
 
 
